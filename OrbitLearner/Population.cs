0554f4aed876b05b0df10ec4e531b4c44125d734
Tyler Hanks <teh.hanks@gmail.com>
2020-06-19 19:03:43 -0400

    start population

---
+++ OrbitLearner/Population.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrbitLearner
+{
+    public class Population
+    {
+        public Population(int size, List<int> brainShape)
+        {
+            members = new List<Brain>(size);
+            for (int i = 0; i < size; i++)
+                members.Add(new Brain(brainShape));
+        }
+
+
+        private List<Brain> members;
+    }
+}
